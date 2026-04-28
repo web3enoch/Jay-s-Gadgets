ac6d15c5cdb787a5002474960e8ab949356fb932
Web3Enoch <web3enoch@gmail.com>
2026-04-28 02:48:57 +0100

    Phase 3

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -2,9 +2,9 @@ import { AuthConfig } from "convex/server";
 
 export default {
   providers: [
-    {
-      domain: process.env.HERCULES_OIDC_AUTHORITY!,
-      applicationID: process.env.HERCULES_OIDC_CLIENT_ID!,
-    },
+  //  {
+    //  domain: process.env.HERCULES_OIDC_AUTHORITY!,
+      //applicationID: process.env.HERCULES_OIDC_CLIENT_ID!,
+   // },
   ],
 } satisfies AuthConfig;
